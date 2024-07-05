157a710ac8a97f2d0e429d2ebf7853ba4e05d37f
Harsh Diwakar <ranjan.harsh215@gmail.com>
2024-07-05 09:31:04 +0000

    edit Application.java for createTour changes

--- tour-service/src/main/java/com/example/Application.java
+++ tour-service/src/main/java/com/example/Application.java
@@ -4,13 +4,15 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.example.tourservice.TravelAgentService;
-
+import com.example.tourservice.TourManagementService;
 public class Application {
 
     public static void main(String[] args) {
 
         ApplicationContext context = new AnnotationConfigApplicationContext(Application.class.getPackageName());
         TravelAgentService agent = context.getBean(TravelAgentService.class);
+        TourManagementService t_agent = context.getBean(TourManagementService.class);
+        t_agent.createTour("Zoo Tour", 100, true);
         System.out.println("\n******Explore California Tour Catalogue******");
         agent.displayTours();
 
